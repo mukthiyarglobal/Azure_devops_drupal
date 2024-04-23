1c21f6e18212a10acf59f30db2a4fc342afdfc03
mukthiyarglobal <145452704+mukthiyarglobal@users.noreply.github.com>
2024-04-23 16:19:48 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,9 +1,25 @@
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "aks_node_rg" {
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
 output "acr_id" {
-  value = azurerm_container_registry.acr_demo.id
+  value = azurerm_container_registry.acr.id
 }
+
 output "acr_login_server" {
-  value = azurerm_container_registry.acr_demo.login_server
+  value = azurerm_container_registry.acr.login_server
 }
-output "webapp_name" {
-  value = azurerm_linux_web_app.webapp_demo.name
+
+resource "local_file" "kubeconfig" {
+  depends_on = [azurerm_kubernetes_cluster.aks]
+  filename   = "kubeconfig"
+  content    = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
